a8a757177e94d6e2e92245c2bfeba144adcec472
naufalrays <rayhannaufalherlano@gmail.com>
2023-06-14 17:59:06 +0700

    Add Landing Page and migration database

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="max-w-7xl grid lg:grid-cols-2 gap-8 mx-auto items-center sm:px-6 lg:px-8 sm:py-2 lg:py-20 mx-auto">
+        <div>
+            <h1 class="text-amber-500 text-3xl font-extrabold">JeWePe Mading</h1>
+            <p class="py-8 dark:text-white">
+                JeWePe Mading dirancang untuk mempromosikan kreativitas dan ekspresi individu. Kami menyediakan beragam fitur dan alat yang memungkinkan anggota komunitas untuk berbagi pemikiran, ide, dan karya kreatif mereka. Apakah itu tulisan, gambar, video, atau bahkan musik, di JeWePe, segala bentuk ekspresi diberi tempat yang pantas!
+            </p>
+            <button class="bg-amber-500 rounded-xl px-6 py-2 text-white">
+                See Articles
+            </button>
+        </div>
+        <img class="rounded-lg" src="images/mading.jpg" alt="">
+    </div>
+</x-app-layout>
